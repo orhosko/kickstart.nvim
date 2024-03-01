875846f808f565ba857d6020bfa72aa8141317c0
orhosko <yilmazor21@itu.edu.tr>
2024-03-01 17:15:10 +0300

    Add copilot and change colorscheme

--- init.lua
+++ init.lua
@@ -700,15 +700,28 @@ require('lazy').setup({
     end,
   },
 
+<<<<<<< HEAD
   {
     'ellisonleao/gruvbox.nvim',
     name = 'gruvbox',
+=======
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'catppuccin/nvim',
+>>>>>>> 1088be0 (Add copilot and change colorscheme)
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+<<<<<<< HEAD
       vim.cmd.colorscheme 'gruvbox'
+=======
+      vim.cmd.colorscheme 'catppuccin-latte'
+>>>>>>> 1088be0 (Add copilot and change colorscheme)
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
